baf256060d890d0c10ce38600967526d7a192795
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-04-28 15:23:03 +0530

    initial commit

---
+++ __tests__/header_test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'react-native'
+import Header from '../components/Header'
+import {render} from '@testing-library/react-native'
+
+describe('header', () => {
+  it('should display title', () => {
+    // const {getByText} = render(
+    //   <Header title="Contacts" searchBar={true} addButton={true} />
+    // )
+    // expect(getByText('Contacts')).toBeTruthy()
+  })
+  it('should display search bar', () => {})
+  it('should display contacts title', () => {})
+  it('should display add button', () => {})
+})
