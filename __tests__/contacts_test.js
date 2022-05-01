3a3de602efc45fdd613eb35df96edbc5ea19da44
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-05-01 09:17:52 +0530

    TDD(RED): Add failed test: should display No contacts to display when there are no contacts

--- __tests__/contacts_test.js
+++ __tests__/contacts_test.js
@@ -3,13 +3,17 @@ import { render } from '@testing-library/react-native'
 import Contacts from '../components/Contacts'
 
 describe('Contacts', () => {
-  test('should display Name as heading', () => {
-    const contacts = []
-    const { queryByText } = render(<Contacts items={contacts} />)
+  test('should display Name as heading when no prop', () => {
+    const { queryByText } = render(<Contacts />)
     expect(queryByText('Name')).toBeTruthy()
   })
-  test('should display heading provided by props', () => {
+  test('should display heading provided in props', () => {
     const { queryByText } = render(<Contacts heading='Contacts Heading' />)
     expect(queryByText('Contacts Heading')).toBeTruthy()
+  })
+  test('should display No contacts to display if no contacts in props', () => {
+    const { queryByText } = render(<Contacts contacts={[]} />)
+    const textNoContacts = queryByText('No Contacts to display')
+    expect(textNoContacts).toBeTruthy()
   })
 })
