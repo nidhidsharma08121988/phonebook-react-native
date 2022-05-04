e9204917f577cd9cf0ae2ff988da10bd493a8f4a
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-05-04 11:50:50 +0530

    TDD(GREEN): Pass test should return empty array if get request fails

--- network_calls/apiCalls.js
+++ network_calls/apiCalls.js
@@ -1,3 +1,12 @@
 import axios from 'axios'
+import { BASE_URL } from './../network_calls/url'
 
-export const getContactsFromApi = () => {}
+export const getContactsFromApi = async () => {
+  try {
+    const res = await axios.get(`${BASE_URL}/Contacts`)
+    const data = await res.data
+    return data
+  } catch (error) {
+    return []
+  }
+}
