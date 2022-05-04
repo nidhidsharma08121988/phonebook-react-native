2d51abb925328736103d1cdb52628548171fb26b
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-05-04 10:39:01 +0530

    Intermediate: Set up json server for our front end and create db.json file as our fake database

---
+++ __tests__/apiCalls_test.js
@@ -0,0 +1,6 @@
+import { getContactsFromApi } from '../network_calls/apiCalls'
+
+jest.mock('fetch')
+describe('API Calls', () => {
+  test('should throw an error when the fetch throws error', () => {})
+})
--- store/ContactState.js
+++ store/ContactState.js
@@ -1,19 +1,22 @@
 import React, { createContext, useReducer } from 'react'
 import contactReducer from './contactReducer'
 
-const initialState = {
-  contacts: [
-    { id: '1', name: 'Pahi', phone: '+91-8175323462' },
-    { id: '2', name: 'Pari', phone: '+91-8175323562' },
-    { id: '3', name: 'Ishaan', phone: '+91-7175323562' },
-  ],
-}
+const initialState = {}
 export const ContactContext = createContext(initialState)
 
 const ContactState = ({ children }) => {
   const [state, dispatch] = useReducer(contactReducer)
+  const fetchContacts = () => {
+    const data = getContactsFromApi()
+    dispatch({
+      type: SET_FETCHED_CONTACT,
+      payload: data,
+    })
+  }
   return (
-    <ContactContext.Provider value={{ contacts: state.contacts }}>
+    <ContactContext.Provider
+      value={{ contacts: state.contacts, fetchContacts }}
+    >
       {children}
     </ContactContext.Provider>
   )
