8af63c697260747ef2fe2b67807ad1a77035e1bb
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-05-06 18:03:59 +0530

    Test(fix): fix the contacts_test to mock fetch and to check if the contacts are displayed correctly on the UI

--- __tests__/contacts_test.js
+++ __tests__/contacts_test.js
@@ -1,9 +1,91 @@
 import React from 'react'
-import { render } from '@testing-library/react-native'
+import { render, waitFor } from '@testing-library/react-native'
 import Contacts from '../components/Contacts'
 import ContactState from '../store/ContactState'
+import fetchMock from 'jest-fetch-mock'
+
+fetchMock.enableMocks()
+
+const contacts = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+      geo: {
+        lat: '-37.3159',
+        lng: '81.1496',
+      },
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+      name: 'Romaguera-Crona',
+      catchPhrase: 'Multi-layered client-server neural-net',
+      bs: 'harness real-time e-markets',
+    },
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv',
+    address: {
+      street: 'Victor Plains',
+      suite: 'Suite 879',
+      city: 'Wisokyburgh',
+      zipcode: '90566-7771',
+      geo: {
+        lat: '-43.9509',
+        lng: '-34.4618',
+      },
+    },
+    phone: '010-692-6593 x09125',
+    website: 'anastasia.net',
+    company: {
+      name: 'Deckow-Crist',
+      catchPhrase: 'Proactive didactic contingency',
+      bs: 'synergize scalable supply-chains',
+    },
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net',
+    address: {
+      street: 'Douglas Extension',
+      suite: 'Suite 847',
+      city: 'McKenziehaven',
+      zipcode: '59590-4157',
+      geo: {
+        lat: '-68.6102',
+        lng: '-47.0653',
+      },
+    },
+    phone: '1-463-123-4447',
+    website: 'ramiro.info',
+    company: {
+      name: 'Romaguera-Jacobson',
+      catchPhrase: 'Face to face bifurcated interface',
+      bs: 'e-enable strategic applications',
+    },
+  },
+]
 
 describe('Contacts', () => {
+  beforeEach(() => {
+    fetchMock.resetMocks()
+  })
+  afterEach(() => {
+    fetchMock.resetMocks()
+  })
+
   test('should display Name as heading when no prop', () => {
     const { queryByText } = render(
       <ContactState>
@@ -21,28 +103,28 @@ describe('Contacts', () => {
     expect(queryByText('Contacts Heading')).toBeTruthy()
   })
   test('should display No contacts to display if no contacts are received', () => {
+    fetchMock.mockReject('Network error')
+
     const { queryByText } = render(
-      <ContactState value={{ contacts: [] }}>
+      <ContactState>
         <Contacts />
       </ContactState>
     )
     const textNoContacts = queryByText(/No contacts to display/i)
     expect(textNoContacts).toBeTruthy()
   })
-  test('should display list of contacts if contacts are received by component', () => {
-    const contacts = [
-      { id: '1', name: 'Pahi', phone: '+91-8175323462' },
-      { id: '2', name: 'Pari', phone: '+91-8175323562' },
-      { id: '3', name: 'Ishaan', phone: '+91-7175323562' },
-    ]
-    const mockFetch = jest.fn()
+  test('should display list of contacts if contacts are received by component', async () => {
+    fetchMock.mockResponse(() => Promise.resolve(JSON.stringify(contacts)))
+
     const { queryAllByTestId } = render(
-      <ContactState value={{ contacts: contacts, fetchContacts: mockFetch }}>
+      <ContactState value={{ contacts: contacts }}>
         <Contacts />
       </ContactState>
     )
-    const contactItems = queryAllByTestId('contact-item')
-    expect(contactItems.length).toBe(3)
-    expect(mockFetch.toHaveBeenCalledOnce).toBeTruthy()
+
+    await waitFor(() => {
+      const contactItems = queryAllByTestId('contact-item')
+      expect(contactItems.length).toBe(3)
+    })
   })
 })
